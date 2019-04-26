1d1254debd1208c6fbee331cd5187c2703e483a4
Grzegorz Musialik <grzegorz.musialik@o2.pl>
2019-04-26 20:36:22 +0200

    Added register form

--- src/app/features/auth/register/register.component.ts
+++ src/app/features/auth/register/register.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+
+import { FormsService } from '../../../core/services/forms.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  public registerForm = this.fb.group({
+    email: [ '', [ Validators.email, Validators.required ]],
+    password: [ '', Validators.required ],
+    confirmPassword: [ '', Validators.required ],
+  });
 
-  ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private formsService: FormsService,
+  ) { }
+
+  public getFormControl(name: string): AbstractControl {
+    return this.formsService.getFormControl(this.registerForm, name);
   }
 
+  public onSubmit(): void {
+    const { email, password, confirmPassword } = this.registerForm.value;
+  }
 }
