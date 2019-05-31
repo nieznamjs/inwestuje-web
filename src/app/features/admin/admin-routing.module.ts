51812e1c8276569dd016476296838de7ed31d6d2
Grzegorz Musialik <grzegorz.musialik@o2.pl>
2019-05-31 21:22:10 +0200

    Added users list

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminComponent } from './admin.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 const routes: Routes = [
-  { path: '', component: AdminComponent },
+  { path: '', component: AdminComponent, children: [
+      { path: 'users', component: UsersListComponent },
+      { path: '', pathMatch: 'full', redirectTo: '/admin/users' },
+      { path: '**', redirectTo: '/admin/users' },
+    ],
+  },
 ];
 
 @NgModule({
--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -5,11 +5,15 @@ import { AdminComponent } from './admin.component';
 import { SharedModule } from '../../shared/shared.module';
 import { AdminHeaderComponent } from './admin-header/admin-header.component';
 import { MatToolbarModule } from '@angular/material';
+import { AdminNavComponent } from './admin-nav/admin-nav.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
     AdminComponent,
     AdminHeaderComponent,
+    AdminNavComponent,
+    UsersListComponent,
   ],
   imports: [
     SharedModule,
