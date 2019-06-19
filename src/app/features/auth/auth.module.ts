82e46ca2fe510483b37530f93fb09f49ede061b4
Grzegorz Musialik <grzegorz.musialik@o2.pl>
2019-06-19 23:42:03 +0200

    [IW-22] RWD

--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../shared/shared.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
-import { SharedModule } from '../../shared/shared.module';
 import { RegisterComponent } from './register/register.component';
 import { PaymentFormComponent } from './register/payment-form/payment-form.component';
 import { UserDataFormComponent } from './register/user-data-form/user-data-form.component';
